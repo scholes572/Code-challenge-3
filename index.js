6b37189388a33d534f23690f3f5ce2626d48eef8
Agostino <agostinoscholes572@gmail.com>
2025-06-23 01:18:04 +0300

    Got post list element and reset content

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function displayPosts() {
     fetch("http://localhost:3000/posts")
        .then(function(response) {
-        return response.json()
+        return response.json();
     })
+       .then(function(posts) {
+        let postlist = document.getElementById("post list");
+        postlist.innerHTML = "";
+       })
 }
